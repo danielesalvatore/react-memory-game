d9dfe0cc2ce9f691c06d7d8ddf9a88bbefaad36b
Daniele Salvatore <danieleslvtr@gmail.com>
2017-11-27 15:28:36 +0100

    Change API endpoints

--- src/schemas/score.js
+++ src/schemas/score.js
@@ -1,4 +1,4 @@
 import { schema } from 'normalizr';
 
-export const score = new schema.Entity('scores', {});
+export const score = new schema.Entity('scores', {}, {idAttribute: "scoreId"});
 export const arrayOfScore = new schema.Array(score);
